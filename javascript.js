63780efbb6e4462ae228ef2639a5b461e32668c9
MNIZ Fahad <mnizfahad@MNIZs-MacBook-Pro.local>
2020-01-27 04:54:44 +0600

    Logical Operator

--- javascript.js
+++ javascript.js
@@ -10,3 +10,12 @@ yearFahad = now - ageFahad;
 console.log(yearFaisal);
 console.log(yearFahad);
 
+console.log(now/2);
+console.log(now*2);
+
+// Logical Operator
+var faisalOlder = ageFaisal > ageFahad;
+var fahadOlder = ageFaisal < ageFahad;
+
+console.log(faisalOlder);
+console.log(fahadOlder);
