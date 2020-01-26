76a919b178a581ee43ca813c31c315833af8746d
MNIZ Fahad <mnizfahad@MNIZs-MacBook-Pro.local>
2020-01-27 04:50:23 +0600

    Math Operator

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var year, yearFahad, yearFaisal;
+now= 2020;
+ageFahad = 25;
+ageFaisal = 31;
+
+// Math Operator
+yearFaisal = now- ageFaisal;
+yearFahad = now - ageFahad;
+
+console.log(yearFaisal);
+console.log(yearFahad);
+
