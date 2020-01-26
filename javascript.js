60ac9b30aeb35bc73bc262982e527bbf61cbfdcf
MNIZ Fahad <mnizfahad@MNIZs-MacBook-Pro.local>
2020-01-27 04:58:37 +0600

    typeof Operator

--- javascript.js
+++ javascript.js
@@ -13,9 +13,23 @@ console.log(yearFahad);
 console.log(now/2);
 console.log(now*2);
 
+
+
 // Logical Operator
 var faisalOlder = ageFaisal > ageFahad;
 var fahadOlder = ageFaisal < ageFahad;
 
 console.log(faisalOlder);
 console.log(fahadOlder);
+
+
+
+// typeof Operator
+console.log(typeof fahadOlder);
+console.log(typeof ageFahad);
+console.log(typeof 'Faisal is Older than Fahad');
+
+var x;
+console.log(typeof x);
+
+
