19096099f773236dbc0e0151dad4174c591d62b3
MNIZ Fahad <mnizfahad@MNIZs-MacBook-Pro.local>
2020-02-03 04:54:09 +0600

    if else statement

--- javascript.js
+++ javascript.js
@@ -62,5 +62,14 @@ var firstName = 'fahad';
 var civilStatus = 'single';
 
 if (civilStatus === 'married') {
-    console.log(firstName + ' is married! ')
+    console.log(firstName + ' is married! ');
+} else {
+    console.log(firstName + ' will hopefully marry soon :) ');
+}
+
+var isMarried = true;
+if (isMarried) {
+    console.log(firstName + ' is married! ');
+} else {
+    console.log(firstName + ' will hopefully marry soon :) ');
 }
